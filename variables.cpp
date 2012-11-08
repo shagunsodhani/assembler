b226196eb39a6bd708e459669ce08ed50984f6db
Shagun Sodhani <sshagun.sodhani@gmail.com>
2012-11-08 17:52:16 +0530

    Added variables.cpp this contains all the variables

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+/*
+ *This file contains all the global variables to be used throughout the code
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int hex_equ[16]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//Array to map hex with their decimal equivalent;
+
+char deci_equ[16]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};//Array to map decimal with their hex equivalent
